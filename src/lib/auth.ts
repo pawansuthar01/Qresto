67bbc1a1fb221c334f3396eaa88742703ed03edf
Pawan Kumar <168888768+pawansuthar01@users.noreply.github.com>
2025-10-08 16:53:09 +0530

    Updated auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,56 +1,42 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "./db";
 import bcrypt from "bcryptjs";
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
-  pages: {
-    signIn: "/signin",
-  },
   providers: [
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    Credentials({
+    CredentialsProvider({
       name: "Credentials",
       credentials: {
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        if (!credentials?.email || !credentials.password) {
+        if (!credentials?.email || !credentials?.password)
           throw new Error("Invalid credentials");
-        }
 
         const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
-        if (!user || !user.password) {
+        if (!user || !user.password)
           throw new Error("Invalid credentials");
-        }
 
         const isPasswordValid = await bcrypt.compare(
           credentials.password,
           user.password
         );
-
-        if (!isPasswordValid) {
+        if (!isPasswordValid)
           throw new Error("Invalid credentials");
-        }
 
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.name,
-          role: user.role,
-          restaurantId: user.restaurantId,
-          image: user.image,
-        };
+        return user;
       },
     }),
   ],
@@ -73,4 +59,4 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
