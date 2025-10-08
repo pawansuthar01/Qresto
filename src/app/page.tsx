954f3fb0602ae40f081366d964a102307901d29e
Pawan Kumar <168888768+pawansuthar01@users.noreply.github.com>
2025-10-08 17:00:52 +0530

    Updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import { auth } from "@/lib/auth";
 
 export default async function HomePage() {
-  const session = await getServerSession(authOptions);
+  const session = await auth();
 
   if (!session) redirect("/signin");
 
